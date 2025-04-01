cd35d247f899a05d52dcac9b7c708a3f2f85b9a5
rafaelcancelier <145573093+rafaelcancelier@users.noreply.github.com>
2025-04-01 08:53:34 -0300

    01*/04

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao = Document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = Document.getElementById('diminuir-fonte');
+document.addEventListener('DOMContentLoaded', function () {
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 
-    diminuiFonteBotao.addEventListener('click', function(){
+    diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
